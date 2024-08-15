d447e46e190bf468f1c8828d63cc7dc3f266564b
calm.wu <wubo0067@hotmail.com>
2024-08-15 18:19:15 +0800

    add generated file

---
+++ linux-4.18.0-348.7.1.el8_5/include/generated/uapi/linux/version.h
@@ -0,0 +1,7 @@
+#define LINUX_VERSION_CODE 266752
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#define RHEL_MAJOR 8
+#define RHEL_MINOR 5
+#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
+#define RHEL_RELEASE_CODE 2053
+#define RHEL_RELEASE "348.7.1"
---
+++ linux-4.18.0-348.7.1.el8_5/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.18.0-348.7.1.cw.x86_64"
